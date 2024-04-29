7db1711cefdbfe656d5a3f3c6652b8c70cd5fd18
OpenSourcedAdmin <167471663+OpenSourcedAdmin@users.noreply.github.com>
2024-04-29 15:11:47 -0400

    Update switchblade.php

--- switchblade.php
+++ switchblade.php
@@ -6,7 +6,7 @@
 // Webhook: https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
 // BOT API Configuration
-$botToken = "TOKEN"; #<------------------- PUT YOUR TOKEN HERE------------->#
+$botToken = "6308720333:AAHrmcKHZYE0z1gaNFSRPcgC-zVguse79u4"; #<------------------- PUT YOUR TOKEN HERE------------->#
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
